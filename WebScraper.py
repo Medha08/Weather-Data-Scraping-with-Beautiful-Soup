c4af7e0630ae4f4c8a3d39971db211c73c80a420
Medha Pandey <medha8pandey@gmail.com>
2017-06-27 18:00:33 +0530

    Added WebScraper

---
+++ WebScraper.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+page = requests.get("http://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168")
+soup = BeautifulSoup(page.content,'html.parser')
+seven_day = soup.find(id="seven-day-forecast")
+period = seven_day.select(".tombstone-container .period-name")
+period_tags = seven_day.select(".tombstone-container .short-desc ")
+temp = seven_day.select(".tombstone-container .temp")
+periods = [pt.get_text() for pt in period_tags]
+period_days =[d.get_text() for d in period]
+temperature =[t.get_text() for t in temp]
+short_desc =[f["title"] for f in seven_day.select(".tombstone-container img")]
+print (short_desc)
+print(periods)
+print(temperature)
+print (period_days)
+
+
